b06ffdfe42465f15e7bc0ce7bb175b05ddf8fef9
Janwillem10704 <112104271+Janwillem10704@users.noreply.github.com>
2022-08-25 17:35:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 40;
+            int b = 60;
+            int c = a + b;
+
+            Console.WriteLine("Your answer: " + c);
+            Console.ReadKey();
+        }
+    }
+}
